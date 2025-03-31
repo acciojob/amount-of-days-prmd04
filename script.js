18186386f0d8e9fd642e52fa70f51be9b1f81c93
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-03-31 11:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function leapYear(year) {
+	return (year % 400 === 0 || (year % 4 === 0 && year % 100 !==0)?366:365);
+}
+
+let year = prompt("Enter the year ");
+alert(leapYear(year));
